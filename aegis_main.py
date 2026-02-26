2c197bad22692118a66210fba51985bf035ac07c
Tim Jefferies <tim.jefferies@gmail.com>
2026-02-26 09:57:17 +0000

    Print error to stdout after exiting curses

--- aegis_main.py
+++ aegis_main.py
@@ -123,10 +123,10 @@ def cli_main(stdscr, args, password):
                 entries = cli_backend.get_entries(vault_path, password)
                 break
             except cli_backend.CLIError as e:
-                stdscr.clear()
                 stdscr.addstr(0, 0, f"Error: {e}", RED_TEXT_COLOR)
                 stdscr.refresh()
                 curses.endwin()
+                print(f"Error: {e}")
                 return
         elif ch in [8, 127, curses.KEY_BACKSPACE]:
             if pwd_input:
